75a3185864c9f0da8e846f1ea9f452b569fd9ef1
Saatvikbot123 <saatvik.sharma@gmail.com>
2024-06-07 17:33:09 -0400

    Prisma stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import Hub from "./Hub";
 import { VercelClient } from "@vercel/postgres";
+import { prisma } from "./api/db";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await prisma.user.findMany();
+  //await prisma.user.create({ data: { name: "test", complete: false } });
   return (
     <div>
       <Hub></Hub>
       <h1 className="text-3xl px-5">Todos</h1>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
